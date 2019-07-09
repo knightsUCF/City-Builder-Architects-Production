c8661637e9281203bfb0396612e0035166a7fbc3
KnightsUCF <peterk@knights.ucf.edu>
2019-07-09 13:56:18 -0400

    Update MobileControls.cs

--- src/MobileControls.cs
+++ src/MobileControls.cs
@@ -3,12 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MobileControls : MonoBehaviour {
+
+
+    // touch rotation
+
+    private Vector3 firstpoint;
+    private Vector3 secondpoint;
+    private float xAngle = 0.0f; //angle for axes x for rotation
+    private float yAngle = 0.0f;
+    private float xAngTemp = 0.0f; //temp variable for angle
+    private float yAngTemp = 0.0f;
+
+    // zoom and pan
+
     Vector3 touchStart;
     public float zoomOutMin = 1;
     public float zoomOutMax = 8;
+
+
+
+
+    void Rotate()
+    {
+        //Check count touches
+        if(Input.touchCount > 0)
+        {
+            //Touch began, save position
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                firstpoint = Input.GetTouch(0).position;
+                xAngTemp = xAngle;
+                yAngTemp = yAngle;
+            }
+
+            //Move finger by screen
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                secondpoint = Input.GetTouch(0).position;
+                //Mainly, about rotate camera. For example, for Screen.width rotate on 180 degree
+                xAngle = xAngTemp + (secondpoint.x - firstpoint.x) * 180.0f / Screen.width;
+                yAngle = yAngTemp - (secondpoint.y - firstpoint.y) * 90.0f / Screen.height;
+                //Rotate camera
+                this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+            }
+        }
+    }
+
+
+
+    void Start()
+    {
+        //Initialization our angles of camera
+        xAngle = 0.0f;
+        yAngle = 0.0f;
+        this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+    }
+
+
+
+
+
+
+
+
+
+    
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Rotate(); 
+
         if(Input.GetMouseButtonDown(0)){
             touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
@@ -38,83 +103,62 @@ public class MobileControls : MonoBehaviour {
 }
 
 
-// https://pressstart.vip/tutorials/2018/07/12/44/pan--zoom.html
-// https://www.youtube.com/watch?v=0G4vcH9N0gc
 
 
-/* typical two finger rotation
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-var rotating: boolean;
-var startVector: Vector2;
-var rotGestureWidth: float;
-var rotAngleMinimum: float;
- 
-function Update () {
-    if (Input.touchCount == 2) {
-        if (!rotating) {
-            startVector = Input.GetTouch(1).position - Input.GetTouch(0).position;
-            rotating = startVector.sqrMagnitude > rotGestureWidth * rotGestureWidth;
-        } else {
-            var currVector = Input.GetTouch(1).position - Input.GetTouch(0).position;
-            var angleOffset = Vector2.Angle(startVector, currVector);
-            var LR = Vector3.Cross(startVector, currVector);
-           
-            if (angleOffset > rotAngleMinimum) {
-                if (LR.z > 0) {
-                    // Anticlockwise turn equal to angleOffset.
-                } else if (LR.z < 0) {
-                    // Clockwise turn equal to angleOffset.
-                }
-            }
-           
+
+public class MobileControls : MonoBehaviour 
+{
+
+    public float speed;
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, -35.0f, 25.0f),
+                Mathf.Clamp(transform.position.y, -1.0f, 20.0f),
+                Mathf.Clamp(transform.position.z, -9.0f, 15.0f)
+            );
         }
-       
-    } else {
-        rotating = false;
+
+        // need else if to prevent double movement 
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.fieldOfView += deltaMagnitudeDiff * 0.1f; // can use speed here for the variable
+
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 15.0f, 70.0f);
+
+
+        }
     }
+
+    
 }
 
-// https://forum.unity.com/threads/rotation-gesture.87308/
 
-// more readable, does this include the fix right before this post?
 
-/// Flag set to true if the user currently makes an rotation gesture, otherwise false
-    private bool rotating = false;
-    /// The squared rotation width determining an rotation
-    public const float TOUCH_ROTATION_WIDTH = 1; // Always
-    /// The threshold in angles which must be exceeded so a touch rotation is recogniced as one
-    public const float TOUCH_ROTATION_MINIMUM = 1;
-    /// Start vector of the current rotation
-    Vector2 startVector = Vector2.zero;
- 
-    /// Processes input for touch rotation, only the first two touches are used
-    private void TouchRotation (){
-        if (Input.touchCount == 2) {
-            if (!rotating) {
-                startVector = Input.touches [1].position - Input.touches [0].position;
-                rotating = startVector.sqrMagnitude > TOUCH_ROTATION_WIDTH;
-            } else {
-                Vector2 currVector = Input.touches [1].position - Input.touches [0].position;
-                float angleOffset = Vector2.Angle(startVector, currVector);
- 
-                if (angleOffset > TOUCH_ROTATION_MINIMUM) {
-                    Vector3 LR = Vector3.Cross(startVector, currVector); // z > 0 left rotation, z < 0 right rotation
- 
-                    if(LR.z > 0)
-                        mouseLook.y += angleOffset;
-                    else if(LR.z < 0)
-                        mouseLook.y -= angleOffset;
-                 
-                    mouseLook.y = Mathf.Clamp (mouseLook.y, 0, 180F); // Clamp looking down and up
- 
-                    GameController.Instance.mainCamera.transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
-                    startVector = currVector;
-                }
-            }
-        } else
-            rotating = false;
-    }
-    
 */
 
 
