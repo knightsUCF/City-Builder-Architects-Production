52e1b501a207ca530403c07b4d1f663fb987d717
KnightsUCF <peterk@knights.ucf.edu>
2019-07-16 21:47:00 -0400

    traffic system notes

--- src/Traffic/TrafficSystem.cs
+++ src/Traffic/TrafficSystem.cs
@@ -2,12 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class TrafficSystem : MonoBehaviour
 {
     
     public void UpdateTrafficSystem()
     {
         // gets called every time we finalize a traffic tile
+
+        // we want to recalculate the traffic paths, and then run the appropriate traffic methods
+
+        // all of our data is in Data.map
+
+        // so now we want to trace this map data to determine the vectors of traffic
+
+
+        // like Carmack said, there is no secret efficient algo, so just write this from scratch
+
+
+        // first let's determine a simple line, the simplest traffic lane we can build
+
+
+        // let's determine, the start, the middle, and the end, and then populate traffic
+
+        // we will have to do intersections next, but just one thing at a time for now
+
+        // also how will the systme react if we add new roads while the cars are already moving
+
+
+        
+
+
+
+
 
 
     }
