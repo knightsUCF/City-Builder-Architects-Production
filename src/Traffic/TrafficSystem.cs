73a49f8a4d7769c41955cca5020d8e8a0f41807d
KnightsUCF <peterk@knights.ucf.edu>
2019-07-17 14:38:55 -0400

    first traffic system working

--- src/Traffic/TrafficSystem.cs
+++ src/Traffic/TrafficSystem.cs
@@ -11,8 +11,23 @@ public class TrafficSystem : MonoBehaviour
     Build build;
 
 
-    Vector2 startWaypoint;
-    Vector2 endWaypoint;
+    public Vector2 startWaypoint;
+    public Vector2 endWaypoint;
+
+
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+
+    float startX, startZ, endX, endZ;
+
+    public GameObject cars;
+
+    public GameObject car1;
+    public GameObject car2;
+
+
+
+
 
 
     void Awake()
@@ -66,14 +81,63 @@ public class TrafficSystem : MonoBehaviour
 
         startWaypoint = Data.roadMap[0];
 
-
-        Debug.Log("start junction: " + startWaypoint);
-
-
         endWaypoint = Data.roadMap[build.roadTileIndex];
 
         
-        Debug.Log("end junction: " + endWaypoint);
+        Vector3 startPos = new Vector3(startWaypoint.x, 0.0f, startWaypoint.y);
+        Vector3 endPos = new Vector3(endWaypoint.x, 0.0f, endWaypoint.y);
+
+        waypoint1.transform.position = startPos;
+        waypoint2.transform.position = endPos;
+
+        // let's activate the cars
+
+        if (cars.activeSelf == false)
+        {
+            cars.SetActive(true);
+
+            // set starting pos of the cars
+
+            car1.transform.position = startPos;
+            car2.transform.position = startPos;
+
+
+        }
+
+
+
+
+        /*
+        startX = waypoint1.transform.position.x;
+        startZ = waypoint1.transform.position.z;
+
+        endX = waypoint2.transform.position.x;
+        endZ = waypoint2.transform.position.z;
+
+        startX = startWaypoint.x;
+        startZ = startWaypoint.y;
+
+        endX = endWaypoint.x;
+        endZ = endWaypoint.y;
+        */
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         // the difference in x, (y is constant), is our vector waypoint
@@ -95,7 +159,7 @@ public class TrafficSystem : MonoBehaviour
 
         // 1. create a GameObject called "Waypoints Holder"
         // 2. attach WaypointsHolder script
-        // 3. under the Waypoints Holder parent, create a game object for each way point
+        // 3. under the Waypoints Holder parent, create a game object for each way point, each one gets the "Waypoint" tag
         // 4. a waypoint is a cube with the mesh renderer turned off, and the box collider turned on and set to trigger
         // 5. attach a "waypoint" script to each of these cube waypoints
 
@@ -113,6 +177,7 @@ public class TrafficSystem : MonoBehaviour
         // 1. Create parent "Junction" game object
         // 2. Attach "Junction Controller" script to the parent Junction game object
         // 3. In the example there are 4 junctions, with trigger 1, script, up to trigger 4 scripts dragged in
+        // 3a. Each trigger 1, trigger 2, etc, gets a "Junction" tag
         // 4. the green light time is 5, and the yellow light time is 2
         // 5. Actually the trigger junctions are game objects with scripts attached...
         // 6. Seems like each trigger is on the right side of the road, right by the lights
@@ -130,11 +195,38 @@ public class TrafficSystem : MonoBehaviour
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        // To make cars & junctions work you need to create tags: 'Waypoint' and 'Junction'
+
+        // Also you need to create layer: 'Car'
+        // As you get 'Waypoint' tags are used by waypoint triggers, and 'Junction' tags are used by junction triggers
+        // Cars are set on 'Car' layer and in their 'Car AI' script in 'Deceleration Layer' is chosen 'Car' too, 
+        // so car's will see each-other and won't collide/go through each-other.
 
         // Okay, let's start off by dragging the car prefab into the hierarchy, which should already contain:
         // rigidbody, (is kinematic - true), box collider (is trigger checked off), Car AI script, with Waypoints holder 1 transform
 
-        
+
+
+        // S T E P S 
+
+        // 1. Drag car onto the hierarchy (set car to "Car" layer, and also set deceleration to "Car" layer so cars don't collide)
+        // 2. Create Waypoints 1 parent game object
+        // 3. Create cube game object under Waypoints 1 parent game object
+        // 4. Turn off mesh renderer
+        // 5. Check is trigger for box collider
+        // 6. Add "Waypoint" script
+        // 7. Adjust the transform position of the waypoint
+        // 8. Repeat for set of waypoints
+
+
+        // Drag the Waypoints 1 parent game object into the Car waypoints slot
+
+        // Set the start waypoints index on the car to 1
+
+
+
+
+
 
 
 
