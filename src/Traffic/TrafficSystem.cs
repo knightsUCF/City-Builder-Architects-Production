410156c1e8bc18451b86b3a9e62cf56239c82ce8
KnightsUCF <peterk@knights.ucf.edu>
2019-07-16 20:58:27 -0400

    Create TrafficSystem.cs

---
+++ src/Traffic/TrafficSystem.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficSystem : MonoBehaviour
+{
+    
+    public void UpdateTrafficSystem()
+    {
+        // gets called every time we finalize a traffic tile
+
+
+    }
+
+
+
+}
