db10f9dde41a55d2ad333d4df44b82df2565c498
KnightsUCF <peterk@knights.ucf.edu>
2019-08-12 23:00:38 -0400

    added wood

--- src/Settings.cs
+++ src/Settings.cs
@@ -12,8 +12,12 @@ public class Settings : Singleton<Settings>
 
     public static int startingMoney = 100;
     public static int startingEnergy = 0;
+    public static int startingWood = 0;
 
     public static int hireWorkerCost = 10;
+
+
+    public static float buildTime = 2.0f;
 
 
     // building costs
