18c9aedf8cf4a5e2791ba0065e0450ad67fd7036
KnightsUCF <peterk@knights.ucf.edu>
2019-08-16 11:32:34 -0400

    mac windows fix

--- src/Worker.cs
+++ src/Worker.cs
@@ -174,7 +174,7 @@ public class Worker : MonoBehaviour
 
     void MoveWorker()
     {
-        #if UNITY_EDITOR || UNITY_EDITOR_OSX || UNITY_EDITOR_WIN
+        #if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN
         
         if (Input.GetMouseButtonDown(0) && mm.currentlySelectedWorkerID == ID)
         {
@@ -213,7 +213,7 @@ public class Worker : MonoBehaviour
     void SetDestination()
     {
 
-        #if UNITY_EDITOR || UNITY_EDITOR_OSX || UNITY_EDITOR_WIN
+        #if UNITY_EDITOR || UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN
 
         ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hitInfo))
