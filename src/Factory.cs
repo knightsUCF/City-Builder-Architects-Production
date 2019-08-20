fffb74e4b96747adfe628b115b7cab8941ca7d9f
KnightsUCF <peterk@knights.ucf.edu>
2019-08-20 14:59:03 -0400

    Create Factory.cs

---
+++ src/Factory.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Factory : MonoBehaviour
+{
+
+    public bool producingEnergy = false;
+
+
+    public int workerID = -1;
+
+    public int workerEngineeringLevel = 0; // we would like to grab the workers engineering level
+
+
+}
