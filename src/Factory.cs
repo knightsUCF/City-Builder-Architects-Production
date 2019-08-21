032a852d8f3efd33d0de29553cfd0db023e61f9c
KnightsUCF <peterk@knights.ucf.edu>
2019-08-21 12:07:17 -0400

    workers info panel

--- src/Factory.cs
+++ src/Factory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 
@@ -8,12 +9,42 @@ using UnityEngine;
 public class Factory : MonoBehaviour
 {
 
+    
+    public GameObject factoryInfoPanel;
+    public Text level;
+    
+    
     public bool producingEnergy = false;
-
-
     public int workerID = -1;
-
     public int workerEngineeringLevel = 0; // we would like to grab the workers engineering level
+
+    bool panelToggle = true;
+
+
+
+
+    void TogglePanel()
+    {
+        factoryInfoPanel.SetActive(panelToggle);
+        panelToggle = !panelToggle;
+    }
+
+
+
+    void OnMouseDown()
+    {
+        UpdateLevel();
+        TogglePanel();
+    }
+
+
+
+    public void UpdateLevel()
+    {
+        level.text = "Level: " + workerEngineeringLevel.ToString();
+        // this is getting updated from PayOut.cs, not sure if this is too messy
+    }
+
 
 
 }
