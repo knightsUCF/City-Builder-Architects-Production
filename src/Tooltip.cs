e40e7b821e65955115d3f56b057cf5b634455510
KnightsUCF <peterk@knights.ucf.edu>
2019-08-21 11:46:36 -0400

    Update Tooltip.cs

--- src/Tooltip.cs
+++ src/Tooltip.cs
@@ -9,12 +9,26 @@ using UnityEngine.UI;
 
 // mouse over does not work with GUI elements
 
+/*
+
+Use
+
+- place Tooltip code on the Button element
+- under the button create a game object "Tooltip panel"
+- under the game object create a panel, and text
+- center the rect transform, don't have to do anything else
+- drag to where needed
+
+*/
+
+
+
 
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     
     public GameObject panel;
-    public Text text;
+    // public Text text;
 
     
 
