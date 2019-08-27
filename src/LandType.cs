79661c02507ac5481a70de9d7128e650d4180026
KnightsUCF <peterk@knights.ucf.edu>
2019-08-27 14:07:37 -0400

    Create LandType.cs

---
+++ src/LandType.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+public class LandType : MonoBehaviour
+{
+
+    // [System.Serializable]
+
+    
+    public enum landType
+    {
+        Residential,
+        Commercial,
+        Industrial
+    };
+
+    public landType zoneType;
+
+
+
+
+}
