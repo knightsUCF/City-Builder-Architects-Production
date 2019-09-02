f92db9be6fab29ca904630e0a34fc2080b0ae006
KnightsUCF <peterk@knights.ucf.edu>
2019-09-02 10:25:50 -0400

    Update KeyboardManager.cs

--- src/KeyboardManager.cs
+++ src/KeyboardManager.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 
 
-
-
 public class KeyboardManager : MonoBehaviour
 {
+
+
+    bool cameraToggle = false;
+
+
 
     void Update()
     {
         if (Input.GetKey("escape") || Input.GetKeyDown(KeyCode.Q))
         {
             Application.Quit();
+        }
+
+
+        // switch cameras
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+
+            if (!cameraToggle) EventManager.TriggerEvent("Toggle to car camera");
+
+            else if (cameraToggle) EventManager.TriggerEvent("Toggle to build camera");
+
+            cameraToggle = !cameraToggle;
+
+            // Debug.Log("camera toggle:");
+            
         }
     }
 }
