8485c45370bc7e023b81fa1ee2138c635f0b7b07
KnightsUCF <peterk@knights.ucf.edu>
2019-09-02 10:24:37 -0400

    Create CarUserControl.cs

---
+++ src/Car/CarUserControl.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+
+        // joystick buttons: https://docs.unity3d.com/Manual/iphone-joystick.html
+
+        private void FixedUpdate()
+        {
+            // pass the input to the car!
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+            // float speed = CrossPlatformInputManager.GetAxis("Fire1");
+
+
+
+            // float break = CrossPlatformInputManager.GetAxis("joystick button 9");
+
+
+
+
+            m_Car.Move(h, v, v, 0.0f);
+
+
+#if !MOBILE_INPUT
+            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(h, v, v, handbrake);
+#else
+            m_Car.Move(h, v, v, 0f);
+#endif
+        }
+    }
+}
