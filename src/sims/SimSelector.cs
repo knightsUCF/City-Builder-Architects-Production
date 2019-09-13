c3cb0cc37b3f47e59959439de28fda839343639f
KnightsUCF <peterk@knights.ucf.edu>
2019-09-12 23:00:28 -0400

    Create SimSelector.cs

---
+++ src/sims/SimSelector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class SimSelector : MonoBehaviour
+{
+
+
+    public GameObject panel;
+
+    bool toggle = true;
+
+    
+
+    void OnMouseDown()
+    {
+
+        panel.SetActive(toggle);
+
+        toggle = !toggle;
+
+        Debug.Log(simData.level);
+    }
+
+}
