05e48acee28f57037b3bf69f15dad7ae9df91bb0
KnightsUCF <peterk@knights.ucf.edu>
2019-09-15 23:15:03 -0400

    Create AI.cs

---
+++ src/sims/AI.cs
@@ -0,0 +1,99 @@
+
+
+
+
+
+// place component on the agent object to give them artificial (synthetic) intelligence
+
+
+
+/*
+    
+
+    - sims approach within range
+
+    - randomized speed within range
+
+    - randomized animations
+
+    - probability based behaviour
+
+    - make decisions: state machine + probability based personality
+
+    - hard code sequences of patterns, and then trigger sequences instead of individual patterns
+
+    - patterns can be used to select patterns
+
+    - state machine
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+class AI
+{
+
+    public bool run = false;
+
+
+    // state machine
+
+
+    // flags
+
+    public bool WORKING = false;
+    public bool OWN_HOUSE = false;
+    public bool IS_MARRIED = false;
+
+
+
+    // current state
+
+    public int CURRENT_STATE = 0;
+
+    // 0 - idle
+    // 1 - moving to destination
+    // 2 - etc
+
+
+
+
+    public void Initialize()
+    {
+        WORKING = true;
+        OWN_HOUSE = true;
+    }
+
+
+
+    void Update()
+    {
+        Run();
+    }
+
+
+
+    public bool Run()
+    {
+        while(run)
+        {
+            if (CURRENT_STATE == 0)
+            {
+                Debug.Log("Agent in idle");
+            }
+
+            else if (CURRENT_STATE == 1)
+            {
+                Debug.Log("Agent is moving");
+            }
+        }
+    }   
+}
