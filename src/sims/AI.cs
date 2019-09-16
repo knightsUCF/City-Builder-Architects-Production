02cc4b027d1479bf1ac99ad3a467365eadfc39dd
KnightsUCF <peterk@knights.ucf.edu>
2019-09-16 00:06:10 -0400

    Update AI.cs

--- src/sims/AI.cs
+++ src/sims/AI.cs
@@ -1,88 +1,80 @@
 
 /*
 
+USE
+
 AI class for managing the state machine of the sim, and also calling actions on state and on pattern
 
 
 
+SET UP 
+
+- place component on the agent object to give them artificial (synthetic) intelligence
+
+
+GENERAL
+
+
+- sims approach within range
+
+- randomized speed within range
+
+- randomized animations
+
+- probability based behaviour
+
+- make decisions: state machine + probability based personality
+
+- hard code sequences of patterns, and then trigger sequences instead of individual patterns
+
+- patterns can be used to select patterns
+
+- state machine
+
+
+    
+EXAMPLE: Animation as the end goal in an "on pattern action" (PATTERN.MEET_SIM)
+
+
+1. Once done with an action we run RunOnStateActions()
+
+2. We then run our probability decider, or choose a prearranged "pattern"
+
+3. In the first case, let's get a random "pattern"
+
+4. We then check this pattern in RunOnState(Pattern)Actions(), so have a separate one:
+
+
+RunOnStateActions()
+RunOnPatternActions()
+
+
+(figure out how to easily get animations to start experimenting with the state machine, because just an animation at the end of scope could be the end goal in a large number of cases)
+
+
+5. When running RunOnPatternActions():
+
+    - that pattern is run in a simple if (pattern.MEET_SIM == PATTERN.MEET_SIM)
+                                    else if (pattern.SOMETHING == PATTERN.SOMETHING)
+
+
+6. In the if statement we place the simple action method we want to call:
+
+    MeetSim()
+    {
+        // find closes sim's coordinates
+
+        // walk over there and waive to them
+
+        // the animation is the end goal in this example
+    }
+
+
+
+
+    
+    
 */
-
-
-
-// place component on the agent object to give them artificial (synthetic) intelligence
-
-
-
-/*
-    
-
-    - sims approach within range
-
-    - randomized speed within range
-
-    - randomized animations
-
-    - probability based behaviour
-
-    - make decisions: state machine + probability based personality
-
-    - hard code sequences of patterns, and then trigger sequences instead of individual patterns
-
-    - patterns can be used to select patterns
-
-    - state machine
-
-    */
-
-
-
-    /*
-    
-    EXAMPLE: Animation as the end goal in an "on pattern action" (PATTERN.MEET_SIM)
-
-
-    1. Once done with an action we run RunOnStateActions()
-
-    2. We then run our probability decider, or choose a prearranged "pattern"
-
-    3. In the first case, let's get a random "pattern"
-
-    4. We then check this pattern in RunOnState(Pattern)Actions(), so have a separate one:
-
-
-    RunOnStateActions()
-    RunOnPatternActions()
-
-
-    (figure out how to easily get animations to start experimenting with the state machine, because just an animation at the end of scope could be the end goal in a large number of cases)
-
-
-    5. When running RunOnPatternActions():
-
-        - that pattern is run in a simple if (pattern.MEET_SIM == PATTERN.MEET_SIM)
-                                     else if (pattern.SOMETHING == PATTERN.SOMETHING)
-
-
-    6. In the if statement we place the simple action method we want to call:
-
-        MeetSim()
-        {
-            // find closes sim's coordinates
-
-            // walk over there and waive to them
-
-            // the animation is the end goal in this example
-        }
-
-
-
-
-
--
-    
-    
-    
-    */
 
 
 
