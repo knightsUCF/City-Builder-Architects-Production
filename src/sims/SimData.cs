19d12dacbb1d5954c85ba0600632338fc4c52906
KnightsUCF <peterk@knights.ucf.edu>
2019-09-20 21:18:03 -0400

    Update SimData.cs

--- src/sims/SimData.cs
+++ src/sims/SimData.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+/*
+
+When any progress is made, we unlearn and learn anew what we thought we knew before. - Henry David Thoreau
+
+*/
+
+
 
 public class SimData : MonoBehaviour
 {
-
+    public int ID = 1;
+    public string name = "Joe";
     public int level = 1;
-    
+    public bool selected = false;
 }
